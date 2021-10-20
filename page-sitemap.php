fa34a5eefe8cbdc0b6aa8a5c89fdd1cbe6481617
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-10-20 01:13:09 -0400

    Added sitemap

--- page-sitemap.php
+++ page-sitemap.php
@@ -13,8 +13,11 @@ get_header(); ?>
 
     <?php while ( have_posts() ) : the_post(); ?>
       <header class="entry-title"><h1 class="page-title"><?php the_title(); ?></h1></header>
-      <section class="entry-content"><?php the_content(); ?></section>
 
+      <?php if ( get_the_content() ) { ?>
+      <section class="entry-content"><?php the_content(); ?></section>
+      <?php } ?>
+      
       <?php if ( has_nav_menu('sitemap') ) { ?>
       <div id="sitemap-wrap">
         <?php wp_nav_menu( array( 'theme_location' => 'sitemap', 'menu_id' => 'sitemap','container_class'=>'sitemap-links') ); ?>
