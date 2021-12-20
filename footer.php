a0e9c4447a5f45b73266735963b3706d1d8e46d8
accrane <austincrane@gmail.com>
2021-12-20 09:05:21 -0500

    add tracking code

--- footer.php
+++ footer.php
@@ -83,5 +83,14 @@
 	
 </div><!-- #page -->
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215105180-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-215105180-1');
+</script>
 </body>
 </html>
